18d8e5feba7c6fc8ba60fa2be8fa24aaa9f05be6
Noxalus <noxalus@gmail.com>
2015-01-08 21:19:43 +0100

    Rename some folders to match with the package name.

--- VGBT/app/src/main/java/com/noxalus/vgbt/videogameblindtest/activities/MainActivity.java
+++ VGBT/app/src/main/java/com/noxalus/vgbt/videogameblindtest/activities/MainActivity.java
@@ -5,11 +5,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Gravity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -134,7 +131,6 @@ public class MainActivity extends Activity implements GameHelper.GameHelperListe
     }
 
     public void unlockAchievementGPGS(String achievementId) {
-        //gameHelper.getGamesClient().unlockAchievement(achievementId);
         Games.Achievements.unlock(gameHelper.getApiClient(), achievementId);
     }
 
